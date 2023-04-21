a2ef67d888b13bb31e6e72efb1cf733784024b7f
scottwhat <66903412+scottwhat@users.noreply.github.com>
2023-04-21 15:24:47 +1000

    healthcheck added

---
+++ components/healthcheckform.js
@@ -0,0 +1,287 @@
+
+
+
+
+
+import Head from "next/head";
+import Image from "next/image";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+import { motion as m } from "framer-motion";
+
+export default function Form() {
+  const router = useRouter();
+
+  const formik = useFormik({
+    initialValues: {
+      q1: "",
+      q2: "",
+      q3: "",
+      q4: "",
+      q5: "",
+      q6: "",
+      q7: "",
+      q8: "",
+      
+    },
+
+    // validationSchema: Yup.object({
+    //   name: Yup.string()
+    //     .max(20, "Name must be 20 characters or less.")
+    //     .required("Name is required"),
+    //   email: Yup.string()
+    //     .email("Invalid email address")
+    //     .required("Email is required"),
+    //   terms: Yup.array().required("Terms of service must be checked"),
+    // }),
+
+    onSubmit: (values) => {
+      console.log("form submitted");
+      console.log(values);
+      router.push({ pathname: "/", query: values });
+    },
+  });
+
+  return (
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="absolute w-full"
+    >
+
+      <main className="  h-screen items-center flex justify-center">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="bg-white flex rounded-lg w-1/2 font-latoRegular"
+        >
+          <div className="flex-1 text-gray-700  p-20">
+            <h1 className="text-3xl pb-2 font-">
+              Lets do a health check! 👋
+            </h1>
+            <p className="text-lg  text-gray-500">
+              This is based off the ABS Health .... Link here
+              add logo ABS
+            </p>
+            <p className="text-lg  text-gray-500">
+              Submit your answers and they will be analysed against guidelines and ABS statistics
+            </p>
+            <div className="mt-6 ">
+
+
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q1"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 1: How is your overrall health today?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q1"
+                  onChange={formik.handleChange}
+                  value={formik.values.q1}
+                >
+                  <option value="">Great</option>
+                  <option value="">Just okay</option>
+                  <option value="">It could be better</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q2"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 2: How many serves of fruit do you eat daily
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q2"
+                  onChange={formik.handleChange}
+                  value={formik.values.q2}
+                >
+                  <option value="1">0</option>
+                  <option value="2">1</option>
+                  <option value="3">2</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q3"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 3: How many serves of vegetables do you eat Daily?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q3"
+                  onChange={formik.handleChange}
+                  value={formik.values.q3}
+                >
+                  <option value={"1"}>0-3</option>
+                  <option value={"2"}>4-6</option>
+                  <option value={"3"}>6+</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q4"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 4: Do you regulary drink sugary drinks?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q4"
+                  onChange={formik.handleChange}
+                  value={formik.values.q4}
+                >
+                  <option value="1">never</option>
+                  <option value="2">sometimes</option>
+                  <option value="3"></option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q5"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 5: Do you perform 150 minutes of physical activity per week?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q5"
+                  onChange={formik.handleChange}
+                  value={formik.values.q5}
+                >
+                  <option value="1">not even close</option>
+                  <option value="2">almost there</option>
+                  <option value="3">yes and typically more!</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q6"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 6: Are you a smoker?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q6"
+                  onChange={formik.handleChange}
+                  value={formik.values.q6}
+                >
+                  <option value="1">yes</option>
+                  <option value="2">ex-smoker</option>
+                  <option value="3">no</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q7"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 7: how much alcohol do you consumer weekly?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q7"
+                  onChange={formik.handleChange}
+                  value={formik.values.q7}
+                >
+                  <option value="1">More than 10 drinks</option>
+                  <option value="2">less than 5 a week</option>
+                  <option value="3">Never or rarely</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              {/* Question start */}
+              <div className="pb-4">
+                <label
+                  htmlFor="q8"
+                  className="flex wrap block font- text-lg pb-2 max-w-lg "
+                >
+                  Question 8: Do you experience chronic body pains?
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+                  name="q8"
+                  onChange={formik.handleChange}
+                  value={formik.values.q8}
+                >
+                  <option value="1">Severe</option>
+                  <option value="2">Mild</option>
+                  <option value="3">None</option>
+                </select>
+              </div>
+              {/* Question End */}
+
+              
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 font- text-sm text-white py-3 mt-6 rounded-lg w-full"
+              >
+                Submit
+              </button>
+            </div>
+
+        </form>
+      </main>
+    </m.div>
+  );
+}
+
+
+//     TEMPLATE
+//     {/* Question start */}
+//     <div className="pb-4">
+//     <label
+//       htmlFor="q1"
+//       className="flex wrap block font- text-lg pb-2 max-w-lg "
+//     >
+//       Question 1:
+//     </label>
+//     <select
+//       className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-blue-500 focus:ring-blue-500 w-full"
+//       name="q1"
+//       onChange={formik.handleChange}
+//       value={formik.values.q1}
+//     >
+//       <option></option>
+//       <option></option>
+//       <option></option>
+//     </select>
+//   </div>
+//   {/* Question start */}
+    
--- pages/healthcheck.js
+++ pages/healthcheck.js
@@ -2,6 +2,9 @@ import { Inter } from '@next/font/google'
 import Header from "../components/header"
 import Footer from "../components/footer"
 import Link from 'next/link'
+import Form from '../components/healthcheckform'
+//
+
 
 const inter = Inter({ subsets: ['latin'] })
 const handleClick = () => {
@@ -10,22 +13,16 @@ const handleClick = () => {
 export default function Data() {
   return (
     <>
+    <div>
       <Header />
-      <main className="flex grid grid-rows w-full h-auto">
-        <div className="flex grid grid-rows mt-5 flex h-[100%] md:h-[70vh] w-[90%] mx-auto bg-blue-100">
-         <h2 className="flex items-center mx-auto text-2xl font-bold">Health Check</h2>
-         <h4>A short health check supported by ABS data to help you focus on your goals</h4>
-         <h4>Add data link here </h4>
-         <p className="flex items-center w-[80%] mx-auto mt-5 bg-blue-100">
-        </p>
-         <Link href="/">
-          <label className="flex text-cyan-800 justify-center text-xl">
-            <span className="underline ml-1"> Return home</span>
-          </label>
-          </Link>
-        </div>
+      <main className="flex flex-col w-full h-auto">
+      <div className='flex wrap flex-col directoblock flex p-20'>
+      <Form></Form>
+      </div>
       </main>
+      </div>
       <Footer />
-    </>
+      
+      </>
   )
 }
